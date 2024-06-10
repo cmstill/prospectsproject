27596f09a774dacd88cad956db66d9ccdd31cf58
cmstill <cameron.still@gmail.com>
2024-06-10 17:23:38 -0500

    added updating functionality...something with get by id no longer working now.

--- controllers/receivers.controller.js
+++ controllers/receivers.controller.js
@@ -23,17 +23,12 @@ export const createReceiver = async (req, res, next) => {
 export const getReceiver = async (req, res, next) => {
   console.log('Controller: getReceiver');
   console.log('Request Params ID:', req.params.id);
-
-  try {
     const result = await ReceiversCoordinator.getReceiver(req.params.id); // req.params.id here is the reciever by id...req.params is on the request object and hold params and now this has to be awaited becuase it's an async operation becuase I'm accessing a DB
     if (result) { // this checks if result (req.param.id) exists and if so, I get a 200 with result...if not then 404 
       res.status(200).json(result);
     } else {
       res.status(404).json();
     }
-  } catch (ex) {
-    next(ex);
-  }
 };
 
 export const deleteReceiver = async (req, res, next) => {
@@ -46,12 +41,17 @@ export const deleteReceiver = async (req, res, next) => {
 };
 
 export const updateReceiver = async (req, res, next) => {
-  const result = ReceiversCoordinator.updateReceiver(req.params.id, req.body); // has to accept the id of the receiver you want to patch and the body object that you are going to be patching the receiver with
-  if (result) {
-    res.status(200).json(result);
-  } else {
-    res.status(404).json();
+  try {
+    const result = await ReceiversCoordinator.updateReceiver(req.params.id, req.body); // has to accept the id of the receiver you want to patch and the body object that you are going to be patching the receiver with
+    if (result) {
+      res.status(200).json(result);
+    } else {
+      res.status(404).json();
+    }
+  } catch (ex) {
+    next(ex);
   }
 };
+
 // every resource I am supporting will need a function in the controller
 /* ie if I am doing a get HTTP method for excercises, I'll make a getExcercises function and etc. for delete, update etc */
--- models/recievers.model.js
+++ models/recievers.model.js
@@ -2,7 +2,7 @@
 import { db } from '../lib/database.js'; // again mongo here is just a renaming of Database class from the file I'm importing
 import Constants from '../lib/constants.js';
 
-let receivers = [
+const receivers = [
   {
     id: '1',
     name: 'cameron',
@@ -17,35 +17,39 @@ export default class ReceiversModel {
   static getReceivers = async () =>
   // return widgets...this needs to be an async function because we're interacting with a database
 
-		 db.getDb().collection(Constants.WRS_COLLECTION).find({}, { projection: Constants.DEFAULT_PROJECTION }).toArray(); // this is using our Database class we set up lib/database and calling getDb method on that then calling .collection method to speciffy what collection we're working with then .find mongo CRUD method fin because we're getting the documents in our collection
+    db.getDb().collection(Constants.WRS_COLLECTION).find({}, { projection: Constants.DEFAULT_PROJECTION }).toArray(); // this is using our Database class we set up lib/database and calling getDb method on that then calling .collection method to speciffy what collection we're working with then .find mongo CRUD method fin because we're getting the documents in our collection
 
   static createReceiver = async (newReceiver) => { // this is accepting a newReceiver (request object from user) then adding it to the wrs collection in my db
     console.log('t//Model: CreateReceiver');
 
     await db.getDb().collection(Constants.WRS_COLLECTION).insertOne({ newReceiver });
-		delete newReceiver._id; // using delte keyword to delete _id property on newReciever object because insertOne doesnt take a projection so we can't do it there
-		return newReceiver; // these two lines are so I get returned to me what I actually want which is the new receiver I created in this case
+    delete newReceiver._id; // using delte keyword to delete _id property on newReciever object because insertOne doesnt take a projection so we can't do it there
+    return newReceiver; // these two lines are so I get returned to me what I actually want which is the new receiver I created in this case
   };
 
   static getReceiver = (id) => {
-    return db.getDb().collection(Constants.WRS_COLLECTION).findOne({ id }, { projection: Constants.DEFAULT_PROJECTION}); //TODO: test this once I can create widgets
-  };
+		console.log('Model: getReceiver with ID:', id);
+		return db.getDb().collection(Constants.WRS_COLLECTION).findOne({ id }, { projection: Constants.DEFAULT_PROJECTION });
+	};
 
-  static deleteReceiver = (id) => {
-    return db.getDb().collection(Constants.WRS_COLLECTION).deleteOne({ id });
-  };
+  static deleteReceiver = (id) => db.getDb().collection(Constants.WRS_COLLECTION).deleteOne({ id });
 
-  static updateReceiver = (id, receiver) => {
-    const receiverIndex = receivers.findIndex((r) => (r.id === id));
-    if (receiverIndex > -1) {
-      Object.keys(receiver).forEach((key) => {
-        if (key === 'id') {
-          return;
-        }
-        receivers[receiverIndex][key] = receiver[key];
-      });
+  static updateReceiver = async (id, receiver) => {
+    const update = {
+      $set: {},
+    };
 
-      return receivers[receiverIndex];
+    Object.keys(receiver).forEach((key) => {
+      if (key === 'id') {
+        return;
+      }
+      update.$set[key] = receiver[key];
+    });
+
+    const result = await db.getDb().collection(Constants.WRS_COLLECTION).updateOne({ id }, update);
+
+    if (result.matchedCount === 1) {
+      return receiver;
     }
 
     return false;
